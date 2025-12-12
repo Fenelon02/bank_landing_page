382f035d343ed9fdcc1694f17878d9ecf92c7db7
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-12-12 15:57:22 -0300

    feat: create the sizes  for the button element

---
+++ src/app/ui/ButtonSizes.ts
@@ -0,0 +1,8 @@
+export const ButtonSizes = {
+    small: "px-3 py-10/15 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+    xlarge: "px-8 py-4 text-xl"
+}
+
+export type Size = keyof typeof ButtonSizes
