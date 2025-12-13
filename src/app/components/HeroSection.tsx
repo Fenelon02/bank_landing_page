6327549ce45a0113abfd3848f9904bd506b58c7f
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-12-13 15:39:26 -0300

    fix: fix the z-index and unused classes

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -3,9 +3,9 @@ import Button from "../ui/Button"
 
 export default function HeroSection(){
     return(
-        <div className="bg-black relative w-full -cover">
+        <div className="bg-black w-full">
             <Header buttons={["Soluções", "Preços", "Sobre Nós", "Conteúdos"]}/>
-            <div className="grid grid-cols-2 h-[60vh] z-20">
+            <div className="grid grid-cols-2 h-[60vh]">
                <div className="flex flex-col justify-center ml-30">
                     <h1 className="text-6xl text-blue-400 font-bold my-4">Banco Bendis</h1>
                     <p className="text-lg text-amber-50">O banco que mais cresce em toda América Latina</p>
