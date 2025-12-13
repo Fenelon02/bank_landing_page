a0b5b9a0d5d3bcfd5539628a83e9d0fb72040970
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-12-13 15:43:52 -0300

    import the feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import HeroSection from "./components/HeroSection";
-import ValueProposition from "./components/ValueProposition";
+import HeroSection from "./components/HeroSection"
+import ValueProposition from "./components/ValueProposition"
+import FeatureSection from "./components/FeaturesSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
       <ValueProposition/>
+      <FeatureSection/>
     </div>
   );
 }
