ad1d517a32dc99e2d1660d0651e19553b1e8b646
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-12-22 22:16:04 -0300

    feat: create the full marque section (not responsive)

---
+++ src/app/components/MarqueeSection.tsx
@@ -0,0 +1,11 @@
+import Marquee from "./Marquee";
+
+export default function MarqueeSection(){
+    return(
+        <div className="flex flex-col items-center justify-center my-5">
+            <h2 className="text-3xl text-blue-200 font-bold">Nossos Parceiros Comerciais</h2>
+            <Marquee/>
+            <div className="h-1 bg-blue-200 w-full"></div>
+        </div>
+    )
+}
