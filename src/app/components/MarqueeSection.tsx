bb13e4f5b952ad5206b8c828f85d5c9d1f8aef77
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-12-23 01:31:25 -0300

    feat: center the text

--- src/app/components/MarqueeSection.tsx
+++ src/app/components/MarqueeSection.tsx
@@ -3,7 +3,7 @@ import Marquee from "./Marquee";
 export default function MarqueeSection(){
     return(
         <div className="flex flex-col items-center justify-center my-5">
-            <h2 className="text-3xl text-blue-200 font-bold">Nossos Parceiros Comerciais</h2>
+            <h2 className="text-center text-3xl text-blue-200 font-bold">Nossos Parceiros Comerciais</h2>
             <Marquee/>
             <div className="h-1 bg-blue-200 w-full"></div>
         </div>
